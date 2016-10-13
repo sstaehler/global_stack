7f074cf01b72cd1ccb9a735e6ebbf54a35298762
sstaehler <staehler@geophysik.uni-muenchen.de>
2016-10-13 15:28:15 +0200

    Add docstrings + plot tweaking

--- global_stack.py
+++ global_stack.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 '''
 Use instaseis to generate a global stack
+:copyright:
+    Simon Stähler
+:license:
+    GNU General Public License, Version 3
 '''
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,6 +19,21 @@ __all__ = ["global_stack",
 class global_stack():
 
     def __init__(self, db_name, depth_in_m=0.0, ndist=1024, dt=1.0):
+        """
+        Create global stack from an Instaseis database
+
+        Keywords:
+        :type  db_name: string
+        :param db_name: filename for the instaseis database
+        :type depth_in_m: float, optional
+        :param depth_in_m: source depth
+        :type ndist: int, optional
+        :param ndist: Number of distances at which to calculate seismogram. Is
+                      equivalent to X-resolution of resulting stack
+        :type dt: float, optional
+        :param dt: Time step of seismograms. Y-resolutions is approximately
+                   (length of database / dt)
+        """
         self.db_name = db_name
         self.depth_in_m = depth_in_m
         self.ndist = ndist
@@ -20,6 +41,16 @@ class global_stack():
 
         self.stack_R, self.stack_T, self.stack_Z, \
             self.stats, self.db_info = self._calc()
+
+    def __str__(self):
+        string = 'db_name:             %s\n' % self.db_name
+        string += 'depth:               %d m\n' % self.depth_in_m
+        string += 'number of distances: %d\n' % self.ndist
+        string += 'dt:                  %5.3f\n' % self.dt
+        string += 'stack size:          %dx%d' % (self.stack_R.shape[0],
+                                                  self.stack_R.shape[2])
+
+        return string
 
     def _calc(self):
         db = instaseis.open_db(self.db_name)
@@ -66,10 +97,41 @@ class global_stack():
         return stack_R, stack_T, stack_Z, st[0].stats, db.info
 
     def save(self, fnam):
+        """
+        Save global stack to pickle on disk
+
+        Keywords:
+        :type  fnam: string
+        :param fnam: filename to save stack into
+        """
         pickle.dump(self, open(fnam, 'wb'))
 
     def plot(self, waterlevel=1e-5, postfac=2,
-             fnam=None, dpi=96, figsize=(10, 10)):
+             fnam=None, dpi=96, figsize=(10, 10),
+             fontsize=16, **kwargs):
+        """
+        Plot global stack
+
+        Keywords:
+        :type  waterlevel: float
+        :param waterlevel: minimum value of plot
+        :type postfac: float
+        :param postfac: Factor with which stack is multiplied after
+                        normalization. Saturates strong values.
+        :type fnam: string, optional
+        :param fnam: Filename in which stack is saved. If not given, the stack
+                     is shown on screen. Choose type by extension!
+        :type dpi: int
+        :param dpi: Resolution of file to write.
+        :type figsize: tuple of float
+        :param figsize: Size of figure in centimeters
+        :type fontsize: float
+        :param fontsize: Font size for axis labels
+
+        kwargs are legal :class:`~matplotlib.axes.Axes` kwargs
+
+        returns :class:`~matplotlib.figure.Figure` object
+        """
 
         npts = self.stack_R.shape[0]
         ndist = self.stack_R.shape[2]
@@ -86,20 +148,24 @@ class global_stack():
         stack[:, :, 2] = _normalize(stack[:, ::-1, 2]) * postfac
 
         fig = plt.figure(figsize=figsize)
-        ax = fig.add_axes([0.1, 0.1, 0.85, 0.85])
+        ax = fig.add_subplot(111, **kwargs)
 
         ax.imshow(stack, aspect='auto',
                   interpolation='bicubic',
                   origin='lower',
                   extent=(0, 180, 0, float(self.stats.endtime)))
 
-        ax.set_xlabel('distance / degree')
-        ax.set_ylabel('time / seconds')
+        ax.tick_params('both', labelright=True)
+        ax.set_xlabel('distance / degree', fontsize=fontsize)
+        ax.set_ylabel('time / seconds', fontsize=fontsize)
 
         ax2 = ax.twiny()
         planet_circ = self.db_info.planet_radius * 2 * np.pi / 1e3
         ax2.set_xlim(0, planet_circ / 2.)
-        ax2.set_xlabel('distance / km')
+        ax2.set_xlabel('distance / km', fontsize=fontsize)
+
+        ax.tick_params(axis='both', which='major', labelsize=fontsize*0.8)
+        ax2.tick_params(axis='both', which='major', labelsize=fontsize*0.8)
 
         if not fnam:
             plt.show()
@@ -111,6 +177,13 @@ class global_stack():
 
 
 def load(fnam):
+    """
+    Load global stack from pickle on disk
+
+    Keywords:
+    :type  fnam: string
+    :param fnam: filename from which to read stack
+    """
     return pickle.load(open(fnam, 'rb'))
 
 
